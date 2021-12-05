9ea664dd61296125d347039f3473b3becd3f98af
Alextxnk <55585988+Alextxnk@users.noreply.github.com>
2021-12-05 20:52:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const clipboard = new ClipboardJS(".copy-btn");
+  const myToastEl = document.querySelector(".toast");
+  const toastText = myToastEl.querySelector(".toast-body");
+
+  clipboard.on("success", (e) => {
+    toastText.textContent = `Ссылка '${e.text}' скопирована в буфер`;
+
+    const myToast = new bootstrap.Toast(myToastEl);
+    myToast.show();
+
+    e.clearSelection();
+  });
+});
